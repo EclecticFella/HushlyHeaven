93f7c9c3740137fe79f6a9fbdbc964b317cef9f0
EclecticFella <hushlyadmin@protonmail.com>
2025-01-29 11:49:00 -0800

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to Hushly</h1>
-      <p>Your Premier OnlyFans Management Agency</p>
-    </div>
-  );
-}
